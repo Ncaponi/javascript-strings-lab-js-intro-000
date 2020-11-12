26ae37de74709420c0991ca7adcad828f115a513
Nicholas Caponi <iamnickygrey@gmail.com>
2020-11-12 15:55:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ var topic = "space";
 var conversation = `${topic}`;
 var greeting = "Hello, everybody!";
 var specialGuest = "Neil"
+`Hello, ${specialGuest}! High`
+"lets/talk/about/space
+"lets/talk"
+`Hello ${specialGuest}! High ${3 + 2}!`
